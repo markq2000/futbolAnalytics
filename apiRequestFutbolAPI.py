aa56828dbac3ea34b8198ccc3cc4c9e2e71e9581
meda123 <mrt62@cornell.edu>
2021-12-06 12:07:57 -0500

    Added API request file to test API call to fetch transfer data

---
+++ apiRequestFutbolAPI.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+TRANSFER_API_KEY = os.getenv("TRANSFER_API_KEY")
+
+# Testing API request to get seasonal data for FC Barcelona
+url = "https://transfermarket.p.rapidapi.com/transfers/list-by-club"
+querystring = {"id":"131","seasonID":"2000"}
+headers = {
+    'x-rapidapi-host': "transfermarket.p.rapidapi.com",
+    'x-rapidapi-key': TRANSFER_API_KEY
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
