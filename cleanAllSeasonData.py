ee5ae9c450a4d067931412a27fa1a85fdf350af8
“Mark” <markquispe09@gmail.com>
2021-12-09 20:53:55 -0500

    obtained futbol data for 20 years, added new file for clean up.

---
+++ cleanAllSeasonData.py
@@ -0,0 +1,22 @@
+# Goal of file is to pull out only current season incoming transfers dictionary
+
+from transferMarketAPIRequest import allSeasons
+
+#Pull current season data for all years
+
+
+
+
+
+
+# response_text = response
+
+# Get current season
+# current_season = response_text['currentSeason']
+#
+# # Get transfer for current season
+# transferArrivals = current_season['transferArrivals']
+
+# Key gives me dictionary of all arrivals
+# for key in transferArrivals:
+#     print(key)
